372917ed1f8feafbd093dff31735480fe6825697
PtruckStar <69282956+PtruckStar@users.noreply.github.com>
2021-05-30 07:32:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,92 +1,78 @@
 const app = require("express")();
 const ytdl = require("ytdl-core");
-const path = require("path")
+const path = require("path");
 
-app.get("/stream/:videoId", async (req, res) => {
-  const { videoId } = req.params;
-  const isValid = ytdl.validateID(videoId);
+app.get("/stream/:videoId", async(req, res) => {
+  const { videoId } = req.params
+  const isValid = ytdl.validateID(videoId)
 
   if (!isValid) {
-    throw new Error();
+    throw new Error()
   }
 
-  const videoInfo = await ytdl.getInfo(videoId);
-
+  const videoInfo = await ytdl.getInfo(videoId)
   let audioFormat = ytdl.chooseFormat(videoInfo.formats, {
     filter: "audioonly",
-    quality: "highestaudio",
+    quality: "highestaudio"
   });
 
-  const { itag, container, contentLength } = audioFormat;
+  const { itag, container, contentLength } = audioFormat
 
-  // Listing 3.
-  const options = {};
+  var start;
+  var end;
+  var total = 0;
+  var contentRange = false;
+  var content_length = 0;
 
-  let start;
-  let end;
-
-  const range = req.headers.range;
+  var range = req.headers.range;
   if (range) {
-    const bytesPrefix = "bytes=";
-    if (range.startsWith(bytesPrefix)) {
-      const bytesRange = range.substring(bytesPrefix.length);
-      const parts = bytesRange.split("-");
-      if (parts.length === 2) {
-        const rangeStart = parts[0] && parts[0].trim();
-        if (rangeStart && rangeStart.length > 0) {
-          options.start = start = parseInt(rangeStart);
-        }
-        const rangeEnd = parts[1] && parts[1].trim();
-        if (rangeEnd && rangeEnd.length > 0) {
-          options.end = end = parseInt(rangeEnd);
-        }
-      }
-    }
+    var positions = range.replace(/bytes=/, "").split("-");
+    start = parseInt(positions[0], 10);
+    total = contentLength;
+    end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+    var chunksize = (end - start) + 1;
+    contentRange = true;
+    content_length = chunksize;
+  } else {
+    start = 0;
+    end = contentLength;
+    content_length = contentLength;
   }
 
-  res.setHeader("content-type", `audio/${container}`);
+  if (start <= end) {
+    var responseCode = 200;
+    var responseHeader = {
+      "Accept-Ranges": "bytes",
+      "Content-Length": content_length,
+      "Content-Type": `audio/${container}`
+    };
+    if (contentRange) {
+      responseCode = 206;
+      responseHeader["Content-Range"] = "bytes " + start + "-" + end + "/" + total;
+    }
+    res.writeHead(responseCode, responseHeader);
 
-  // Listing 4.
-  if (req.method === "HEAD") {
-    res.statusCode = 200;
-    res.setHeader("accept-ranges", "bytes");
-    res.setHeader("content-length", contentLength);
-    res.end();
+    const range = { start: start, end: end }
+    //const audioStream = ytdl(videoId, { filter: format => format.itag === itag, range })
+
+    var stream = ytdl(videoId, { filter: format => format.itag === itag, range })
+      .on("readable", function() {
+        var chunk;
+        while (null !== (chunk = stream.read(1024))) {
+          res.write(chunk);
+        }
+      }).on("error", function(err) {
+        res.end(err);
+      }).on("end", function(err) {
+        res.end();
+      });
   } else {
-    // Listing 5.
-    let retrievedLength;
-    if (start !== undefined && end !== undefined) {
-      retrievedLength = end + 1 - start;
-    } else if (start !== undefined) {
-      retrievedLength = contentLength - start;
-    } else if (end !== undefined) {
-      retrievedLength = end + 1;
-    } else {
-      retrievedLength = contentLength;
-    }
-
-    // Listing 6.
-    res.statusCode = start !== undefined || end !== undefined ? 206 : 200;
-
-    res.setHeader("content-length", retrievedLength);
-
-    if (range !== undefined) {
-      res.setHeader(
-        "content-range",
-        `bytes ${start || 0}-${end || contentLength - 1}/${contentLength}`
-      );
-      res.setHeader("accept-ranges", "bytes");
-    }
-
-    // Listing 7.
-    const ranges = { start: start, end: end }
-    const audioStream = ytdl(videoId, { filter: format => format.itag === itag, ranges })
-    audioStream.pipe(res)
+    return res.status(403).send();
   }
 });
 
 const playerView = (req, res) => {
-    res.sendFile(path.resolve("player.html"));
+  res.sendFile(path.resolve("player-safari.html"));
 }
 app.get('/', playerView)
 
